de9dd92fa50cab449a5f0418341e70fd336d339f
linhnv296 <linhnv296@gmail.com>
2019-07-01 22:25:42 +0700

    Quan ly khach hang su dung Eloquent va phan trang

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\City;
+use Illuminate\Http\Request;
+
+class CityController extends Controller
+{
+    public function index(){
+        $cities = City::all();
+        return view('cities/index', compact('cities'));
+    }
+
+    public function create(){
+        return view('cities/create');
+    }
+
+    public function store(Request $request){
+        $city = new City();
+        $city->name = $request->input('name');
+        $city->save();
+        return redirect()->route('cities.index');
+    }
+
+    public function edit($id){
+        $city = City::findOrFail($id);
+        return view('cities.edit', compact('city'));
+    }
+
+    public function update(Request $request, $id){
+        $city = City::findOrFail($id);
+        $city->name = $request->input('name');
+        $city->save();
+        return redirect()->route('cities.index');
+    }
+
+    public function delete($id){
+        $city = City::findOrFail($id);
+        $city->delete();
+        return redirect()->route('cities.index');
+    }
+}
